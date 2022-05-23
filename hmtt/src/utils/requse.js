45db612669e5e8fc78bd04c0490b4e072816b0e1
fangtaiteng <657633609@qq.com>
2022-05-23 22:38:02 +0800

    封装axios网络请求

---
+++ hmtt/src/utils/requse.js
@@ -0,0 +1,15 @@
+// 引入axios
+import theAxios from 'axios'
+const axios = theAxios.create({
+  baseURL: 'http://toutiao.itheima.net',
+  timeout: 20000 // 20秒超时时间(请求20秒无响应直接判定超时)
+})
+export default ({ url, method = 'GET', params = {}, data = {}, headers = {} }) => {
+  return axios({
+    url,
+    method,
+    params,
+    data,
+    headers
+  })
+}
